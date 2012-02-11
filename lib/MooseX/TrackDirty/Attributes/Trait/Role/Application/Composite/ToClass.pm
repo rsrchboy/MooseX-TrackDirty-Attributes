fb6f37f016fe85e76bc5af1d677796f45f20dfce
Chris Weyl <cweyl@alumni.drew.edu>
2012-02-11 11:28:39 -0800

    drop old Composite::To* roles

--- lib/MooseX/TrackDirty/Attributes/Trait/Role/Application/Composite/ToClass.pm
+++
@@ -1,9 +0,0 @@
-package MooseX::TrackDirty::Attributes::Trait::Role::Application::Composite::ToClass;
-# Dist::Zilla: +PkgVersion
-
-use Moose::Role;
-use namespace::autoclean;
-
-with 'MooseX::TrackDirty::Attributes::Trait::Role::Application::ToClass';
-
-!!42;
--- lib/MooseX/TrackDirty/Attributes/Util.pm
+++ lib/MooseX/TrackDirty/Attributes/Util.pm
@@ -9,7 +9,6 @@ use Sub::Exporter -setup => {
     exports => [ qw{
         trait_for application
         Composite ToRole ToClass ToInstance
-        CompositeToClass CompositeToRole CompositeToInstance
         AccessorNativeTrait TrackDirtyNativeTrait
     } ],
     groups => { default => ':all' },
@@ -20,13 +19,9 @@ sub application($)          { trait_for "Role::Application::$_[0]"           }
 sub Composite()             { trait_for 'Role::Composite'                    }
 sub ToInstance()            { application 'ToInstance'                       }
 sub ToClass()               { application 'ToClass'                          }
-sub ToRole()                { trait_for 'Role::Application::ToRole'          }
+sub ToRole()                { application 'ToRole'                           }
 sub TrackDirtyNativeTrait() { trait_for 'Attribute::Native::Trait'           }
 sub AccessorNativeTrait()   { trait_for 'Method::Accessor::Native'           }
-sub composite($)            { application "Composite::$_[0]"                 }
-sub CompositeToClass()      { composite 'ToClass'                            }
-sub CompositeToRole()       { composite 'ToRole'                             }
-sub CompositeToInstance()   { composite 'ToInstance'                         }
 
 !!42;
 
